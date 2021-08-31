ec4d98db22c9247869b0ea6882319704c610d73d
Igor Buzykin <lexemz@gmail.com>
2021-08-31 15:21:25 +0300

    работающий вариант

--- PersonalityQuiz/ViewControllers/ResultViewController.swift
+++ PersonalityQuiz/ViewControllers/ResultViewController.swift
@@ -25,33 +25,53 @@ class ResultViewController: UIViewController {
         printResult()
     }
 
-    private func identifyAnimal() -> Animal? {
-        let animalDuplicates = Dictionary(
-            grouping: answersChosen.map { $0.animal },
-            by: { $0 }
-        ).filter { $1.count > 1 }.keys
-
-        answersChosen.forEach { print($0.animal) }
-        
-        // если частое животное одно, его и возвращаем
-        if animalDuplicates.isEmpty {
-            return answersChosen.first?.animal
-        }
-        
-        // если частых животных >1, ищем первое упомянутое животное среди дубликатов
-        for answer in answersChosen {
-            if animalDuplicates.contains(answer.animal) {
-                return answer.animal
-            }
-        }
-
-        return nil
-    }
-    
     private func printResult() {
         if let animal = identifyAnimal() {
             resultTitle.text = "Вы - \(animal.rawValue)"
             resultDescription.text = animal.definition
         }
+    }
+
+    private func identifyAnimal() -> Animal? {
+        let answersDuplicates = Dictionary(
+            grouping: answersChosen.map { $0.animal },
+            by: { $0 }
+        ).filter { $1.count > 1 }
+
+        /* Реализация данного приложения не предусматривает случаи, когда
+         дубликаты ответов есть, но их количество разное.
+         
+         Например:
+         если добавить ещё один вопрос, определяющий каким животным является пользователь,
+         то количество дубликатов может быть >1 и
+         один из дубликатов может встречаться чаще другого:
+         
+         [cat, rabbit, cat, rabbit, rabbit, dog]
+         
+         и cat и rabbit - несколько раз встречаются в ответах, поэтому нужен
+         отсортированный по кол-ву дубликатов масив [rabbit, dog]. */
+        let duplicatesDescendingOrder = answersDuplicates
+            .sorted { $0.1.count > $1.1.count }
+            .map { $0.0 }
+
+        answersChosen.forEach { print($0.animal) }
+        print()
+        print(answersDuplicates.values)
+        print()
+        print(duplicatesDescendingOrder)
+
+        // если дубликатов животных нет в ответах, возвращаем первый выбранный пользователем ответ
+        if answersDuplicates.isEmpty {
+            return answersChosen.first?.animal
+        }
+
+        // если есть дубликаты, ищем первое упомянутое животное среди дубликатов
+        for answer in answersChosen {
+            if duplicatesDescendingOrder.contains(answer.animal) {
+                return answer.animal
+            }
+        }
+
+        return nil
     }
 }
